aa783ad3f4750be1d74eea62525e5cb31084511a
小鸡快跑 <2283572185@qq.com>
2024-01-31 23:54:30 +0800

    添加注释

--- gcc_environment.py
+++ gcc_environment.py
@@ -58,42 +58,76 @@ class environment:
         self.gdbinit_path = os.path.join(self.prefix, "share", ".gdbinit")
 
     def update(self) -> None:
+        """更新源代码
+
+        """
         for lib in ("expat", "gcc", "binutils", "linux", "mingw", "pexports", "glibc"):
             path = os.path.join(self.home_dir, lib)
             os.chdir(path)
             run_command("git pull")
 
-    def enter_build_dir(self, lib: str) -> str:
+    def enter_build_dir(self, lib: str) -> None:
+        """进入构建目录
+
+        Args:
+            lib (str): 要构建的库
+        """
         assert lib in lib_list
         build_dir = os.path.join(self.home_dir, lib, "build" if lib != "expat" else "expat/build")
         if os.path.isdir(build_dir):
             shutil.rmtree(build_dir)
         os.mkdir(build_dir)
         os.chdir(build_dir)
-        return build_dir
 
     def configure(self, *option: str) -> None:
+        """自动对库进行配置
+
+        Args:
+            option (tuple[str, ...]): 配置选项
+        """
         options = " ".join(("", *option))
         run_command(f"../configure {options}")
 
     def make(self, *target: str) -> None:
+        """自动对库进行编译
+
+        Args:
+            target (tuple[str, ...]): 要编译的目标
+        """
         targets = " ".join(("", *target))
         run_command(f"make {targets} -j {self.num_cores}")
 
     def install(self, *target: str) -> None:
-        targets = " ".join(("", *target)) if target != () else "install-strip"
+        """自动对库进行安装
+
+        Args:
+            target (tuple[str, ...]): 要安装的目标
+        """
+        if target != ():
+            targets = " ".join(("", *target))
+        else:
+            run_command(f"make install-strip -j {self.num_cores}")
+            targets = ""
+            for dll in dll_list:
+                targets += f"install-target-{dll} "
         run_command(f"make {targets} -j {self.num_cores}")
 
     def register_in_env(self) -> None:
+        """注册安装路径到环境变量
+        """
         os.environ["PATH"] = self.bin_dir + ":" + os.environ["PATH"]
 
     def register_in_bashrc(self) -> None:
+        """注册安装路径到用户配置文件
+        """
         bashrc_file = io.open(os.path.join(self.home_dir, ".bashrc"), "a")
         bashrc_file.writelines(f"export PATH={self.bin_dir}:$PATH")
         bashrc_file.close()
         self.register_in_env()
 
     def copy_gdbinit(self) -> None:
+        """复制.gdbinit文件
+        """
         gdbinit_src_path = os.path.join(self.current_dir, ".gdbinit")
         shutil.copyfile(gdbinit_src_path, self.gdbinit_path)
 
@@ -103,6 +137,8 @@ class environment:
         shutil.copyfile(readme_path, target_path)
 
     def symlink_multilib(self) -> None:
+        """为编译带有multilib支持的交叉编译器创建软链接，如将lib/32链接到lib32
+        """
         multilib_list = {}
         for multilib in os.listdir(self.lib_prefix):
             if multilib != "lib" and multilib.startswith("lib") and os.path.isdir(os.path.join(self.lib_prefix, multilib)):
@@ -118,6 +154,8 @@ class environment:
         os.chdir(cwd)
 
     def delete_symlink(self) -> None:
+        """删除编译交叉编译器所需的软链接，在完成编译后不再需要这些软链接
+        """
         for symlink in self.symlink_list:
             os.unlink(symlink)
 
