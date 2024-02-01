82d575c5bc20a1cfa794836356ddd105d44b0e11
小鸡快跑 <2283572185@qq.com>
2024-02-01 23:47:30 +0800

    修改脚本以支持--home选项

--- python_config.py
+++ python_config.py
@@ -4,17 +4,23 @@ import sys
 import os
 from gcc_environment import environment
 
-assert len(sys.argv) == 3, "Too many args." if len(sys.argv) > 3 else "Too few args"
-option = sys.argv[2]
-env = environment("")
-python_dir = os.path.join(env.home_dir, "python-embed")
 
-match option:
-    case "--includes":
-        print(f"-I{os.path.join(python_dir, 'include')}")
-    case "--ldflags":
-        print(f"-L{python_dir} -lpython")
-    case "--exec-prefix":
-        print(f"-L{python_dir}")
-    case _:
-        assert False, f'Invalid option "{option}"'
+def get_config() -> None:
+    assert len(sys.argv) >= 3, "Too few args"
+    env = environment("")
+    python_dir = env.lib_dir_list["python-embed"]
+    result_list = {
+        "--includes": f"-I{os.path.join(python_dir, 'include')}",
+        "--ldflags": f"-L{python_dir} -lpython",
+        "--exec-prefix": f"-L{python_dir}",
+    }
+    option_list = sys.argv[2:]
+    for option in option_list:
+        if option in result_list:
+            print(result_list[option])
+            return
+    assert False, f'Invalid option list: {" ".join(option_list)}'
+
+
+if __name__ == "__main__":
+    get_config()
