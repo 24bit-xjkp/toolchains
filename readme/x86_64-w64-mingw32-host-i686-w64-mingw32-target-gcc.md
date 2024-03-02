020f0eb7401066211cf3ca3900ef1960ea080939
小鸡快跑 <2283572185@qq.com>
2024-03-03 00:30:08 +0800

    更新，i686 MinGW

---
+++ readme/x86_64-w64-mingw32-host-i686-w64-mingw32-target-gcc.md
@@ -0,0 +1,21 @@
+# GCC14工具链
+
+## 平台
+
+| build            | host               | target           |
+| :--------------- | :----------------- | :--------------- |
+| x86_64-linux-gnu | x86_64-w64-mingw32 | i686-w64-mingw32 |
+
+## 版本
+
+- GCC：14.0.1
+- Binutils：2.42.50
+- Mingw-w64：10.0.0
+- PExports：0.47
+
+## 组件
+
+- gcc
+- g++
+- binutils
+- pexports
---
+++ script/x86_64_w64_mingw32_host_i686_w64_mingw32_target_gcc.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import gcc_environment as gcc
+from x86_64_linux_gnu_host_i686_w64_mingw32_target_gcc import env as cross_env
+import shutil
+import os
+
+env = gcc.environment("14", host="x86_64-w64-mingw32", target="i686-w64-mingw32")
+
+
+def build() -> None:
+    # 更新源代码
+    # env.update()
+
+    basic_option = f"--disable-werror --prefix={env.prefix} --host={env.host} --target={env.target}"
+    gcc_option = "--disable-multilib --enable-languages=c,c++ --disable-sjlj-exceptions --enable-threads=win32"
+
+    # 编译安装完整gcc
+    env.enter_build_dir("gcc")
+    env.configure(basic_option, gcc_option)
+    env.make()
+    env.install("install-strip")
+
+    # 删除已安装的dll
+    os.chdir(env.bin_dir)
+    for file in os.listdir(env.bin_dir):
+        if file.endswith(".dll"):
+            os.remove(file)
+    # 从交叉工具链复制文件
+    for dir in ("include", "lib"):
+        cross_dir = os.path.join(cross_env.lib_prefix, dir)
+        current_dir = os.path.join(env.lib_prefix, dir)
+        for item in os.listdir(cross_dir):
+            dst_path = os.path.join(current_dir, item)
+            src_path = os.path.join(cross_dir, item)
+            if not os.path.exists(dst_path):
+                shutil.copytree(src_path, dst_path) if os.path.isdir(src_path) else shutil.copyfile(src_path, dst_path)
+
+    # 编译安装binutils
+    env.enter_build_dir("binutils")
+    env.configure(basic_option, "--disable-gdb")
+    env.make()
+    env.install()
+
+    # 编译安装pexports
+    env.enter_build_dir("pexports")
+    env.configure(f"--prefix={env.prefix} --host={env.host}")
+    env.make()
+    env.install()
+    # 添加target前缀
+    os.rename(os.path.join(env.bin_dir, "pexports.exe"), os.path.join(env.bin_dir, f"{env.target}-pexports.exe"))
+    # 打包工具链
+    env.package(False)
+
+
+if __name__ == "__main__":
+    build()
