0fd6fa55b4772b74323fde0ea1ffeb1ef6d01f86
小鸡快跑 <2283572185@qq.com>
2024-03-03 11:46:13 +0800

    更新

--- script/x86_64_w64_mingw32_host_x86_64_linux_gnu_target_gcc.py
+++ script/x86_64_w64_mingw32_host_x86_64_linux_gnu_target_gcc.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import gcc_environment as gcc
-import shutil
-import os
 
 env = gcc.environment("14", host="x86_64-w64-mingw32", target="x86_64-linux-gnu")
 
