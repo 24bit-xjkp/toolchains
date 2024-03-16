e97e9ad9cedb0b1c74fa5db14b328faee3b2a889
24bit-xjkp <2283572185@qq.com>
2024-03-16 19:29:37 +0800

    更新

--- script/x86_64_linux_gnu_clang.py
+++ script/x86_64_linux_gnu_clang.py
@@ -27,8 +27,9 @@ def build(stage: int = env.stage) -> None:
             env.remove_build_dir("runtimes")
         env.stage += 1
 
+    basic_command = ("-stdlib=libc++", "-unwindlib=libunwind", "-rtlib=compiler-rt")
     if env.stage == 2:
-        env.config("llvm", env.host, "-stdlib=libc++ -unwindlib=libunwind -rtlib=compiler-rt", **{**env.dylib_option_list, **env.llvm_option_list_2})
+        env.config("llvm", env.host, *basic_command, **{**env.dylib_option_list, **env.llvm_option_list_2})
         env.make("llvm")
         env.install("llvm")
         env.copy_llvm_libs()
@@ -36,17 +37,17 @@ def build(stage: int = env.stage) -> None:
 
     if env.stage == 3:
         for target in llvm.system_list.keys():
-            command = "-stdlib=libc++ -unwindlib=libunwind -rtlib=compiler-rt "
+            command = basic_command
             match target:
                 case "x86_64-w64-mingw32":
                     option = env.llvm_option_list_w64_3
-                    command += "-lgcc -lunwind -lsupc++"
+                    command = (*basic_command, "-lgcc", "-lunwind,", "-lsupc++")
                 case "i686-w64-mingw32":
                     option = env.llvm_option_list_w32_3
-                    command += "-lgcc -lunwind -lsupc++"
+                    command = (*basic_command, "-lgcc", "-lunwind,", "-lsupc++")
                 case _:
                     option = env.llvm_option_list_3
-            env.config("runtimes", target, command, **option)
+            env.config("runtimes", target, *command, **option)
             env.make("runtimes")
             env.install("runtimes")
             env.build_sysroot(target)
@@ -56,7 +57,6 @@ def build(stage: int = env.stage) -> None:
     if env.stage == 4:
         env.change_name()
         env.package()
-
 
 
 if __name__ == "__main__":
