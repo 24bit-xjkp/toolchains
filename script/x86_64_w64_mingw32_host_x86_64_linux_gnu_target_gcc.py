e3d08bb7dfba6ae620446cc086bdeb1ba4c2aba2
24bit-xjkp <2283572185@qq.com>
2024-07-27 23:03:35 +0800

    fix(script): 修正x86_64-linux-gnu目标支持
    1. 修正x86_64_w64_mingw32_host_x86_64_linux_gnu_target_gcc.py实现以构建完整的工具链，包括linux header和glibc

--- script/x86_64_w64_mingw32_host_x86_64_linux_gnu_target_gcc.py
+++ script/x86_64_w64_mingw32_host_x86_64_linux_gnu_target_gcc.py
@@ -3,6 +3,7 @@
 import gcc_environment as gcc
 from x86_64_w64_mingw32_native_gcc import build_gdb_requirements
 from x86_64_w64_mingw32_host_arm_none_eabi_target_gcc import copy_lib
+import os
 
 env = gcc.environment(host="x86_64-w64-mingw32", target="x86_64-linux-gnu")
 
@@ -14,12 +15,23 @@ def build() -> None:
     basic_option = f"--disable-werror --prefix={env.prefix} --host={env.host} --target={env.target}"
     gcc_option = "--disable-multilib --enable-languages=c,c++"
     binutils_option = f"--with-system-gdbinit={env.gdbinit_path} --disable-gdbserver --with-python={env.python_config_path} CXXFLAGS=-D_WIN32_WINNT=0x0600"
+    glibc_option = f"--prefix={env.lib_prefix} --host={env.target} --build={env.build} --disable-werror"
 
     # 编译安装完整gcc
     env.enter_build_dir("gcc")
     env.configure(basic_option, gcc_option)
     env.make()
     env.install("install-strip")
+
+    # 安装Linux头文件
+    env.enter_build_dir("linux")
+    env.make(f"ARCH=x86 INSTALL_HDR_PATH={env.lib_prefix} headers_install")
+
+    # 安装glibc头文件
+    env.enter_build_dir("glibc")
+    env.configure(glibc_option, "libc_cv_forced_unwind=yes")
+    env.make("install-headers")
+    os.mknod(os.path.join(env.lib_prefix, "include", "gnu", "stubs.h"))
 
     # 创建libpython.a
     env.build_libpython()
@@ -34,8 +46,6 @@ def build() -> None:
 
     # 复制gdb所需运行库
     copy_lib(env)
-    # 复制文件
-    env.copy_from_cross_toolchain()
 
     # 打包工具链
     env.package(need_python_embed_package=True)
